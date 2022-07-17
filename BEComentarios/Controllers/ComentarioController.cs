a056ee1d788e63625ddc7129775ba9e777968d42
ajlf2020 <antoniojlfz2010@gmail.com>
2022-07-17 18:39:58 -0500

    Agregar archivos de proyecto.

---
+++ BEComentarios/Controllers/ComentarioController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BEComentarios.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEComentarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public ComentarioController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Comentario
+        [HttpGet]
+        //public IEnumerable<Comentario> Get()
+        //{
+        //    //return new string[] { "value1", "value2" };
+        //    try
+        //    {
+        //        var ListComentarios =  _context.Comentario.ToList();
+        //        return ListComentarios;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return null;
+        //    }
+        //}
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var ListComentarios = await _context.Comentario.ToListAsync();
+                return Ok(ListComentarios);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // GET: api/Comentario/5
+        [HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var comentario = await _context.Comentario.FindAsync(id);
+                if(comentario == null)
+                {
+                    return NotFound();
+                }
+                return Ok(comentario);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // POST: api/Comentario
+        [HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        public async Task<IActionResult> Post([FromBody] Comentario comentario)
+        {
+            try
+            {
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return Ok(comentario);
+
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // PUT: api/Comentario/5
+        [HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+        public async Task<IActionResult> Put(int id, [FromBody] Comentario comentario)
+        {
+            try
+            {
+                if(id != comentario.Id)
+                {
+                    return BadRequest();
+                }
+                _context.Update(comentario);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Comentario Actualizado con Exito !" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var comentario = await _context.Comentario.FindAsync(id);
+                if (comentario == null)
+                    return NotFound();
+                _context.Comentario.Remove(comentario);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Comentario Eliminado con Exito !" });
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ BEComentarios/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
+
+namespace BEComentarios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            string mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AplicationDbContext>(options =>
+                options.UseMySql(mySqlConnection, mySqlOptions =>
+                {
+                    mySqlOptions.ServerVersion(new Version(2,1,14), ServerType.MySql)
+                    .EnableRetryOnFailure(
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+                }));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+            });
+        }
+
+        private Action<MySqlDbContextOptionsBuilder> ServerVersion(Version version)
+        {
+            throw new NotImplementedException();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors("CorsPolicy");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
